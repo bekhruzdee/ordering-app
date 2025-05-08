b5189a10a6609cfa23e3085c265c31c4ee980964
bekhruzdee <qutbidinovbehruz003@gmail.com>
2025-05-08 12:24:33 +0500

    update orders

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -14,11 +14,12 @@ import { Order, OrderSchema } from './schemas/order.schema';
       isGlobal: true,
       validationSchema: Joi.object({
         MONGODB_URI: Joi.string().required(),
+        PORT: Joi.string().required(),
       }),
       envFilePath: './apps/orders/.env',
     }),
     DatabaseModule,
-    MongooseModule.forFeature([{name: Order.name, schema: OrderSchema}])
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
   ],
   controllers: [OrdersController],
   providers: [OrdersService, OrdersRepository],
